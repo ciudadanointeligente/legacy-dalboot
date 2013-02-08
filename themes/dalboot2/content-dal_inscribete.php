fd10252a99e9c39019aefa97535c6274ce7b9ed5
Montse Lobos <montselobos@gmail.com>
2013-02-08 12:59:37 -0300

    Fix button display on registration page
    also fix text.

--- themes/dalboot2/content-dal_inscribete.php
+++ themes/dalboot2/content-dal_inscribete.php
@@ -1,10 +1,10 @@
 <article>
-	<div class="infoApp row" >
+	<div class="infoApp" >
 		<div class="fullcentered nolist" >
 			
 			<?php 
 			$post_meta_data = get_post_custom($post->ID); 
-			echo'<a class= "btn btn-large btn-action span2" href="'.$post_meta_data[country_inscribete][0].'" ><i class="icon-pencil icon-white"></i>  Sign up on '.get_the_title().'</a>';?>	
+			echo'<a class= "btn btn-large btn-action span2" href="'.$post_meta_data[country_inscribete][0].'" ><i class="icon-pencil icon-white"></i> '.__( 'Participate in ', 'bootstrapwp' ) .''.get_the_title().'</a>';?>	
 		</div>
 	</div>
 </article>	
--- themes/dalboot2/page-inscribete.php
+++ themes/dalboot2/page-inscribete.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *
- * Template Name: Inscribete Page
+ * Template Name: Registration Page
  * 
  * Page template with minimal formatting, a fixed 940px container and right sidebar layout
  *
@@ -13,33 +13,33 @@
 get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
    <div class="container">
-        <h1><?php the_title();?></h1>    
-        <div class="row content">
-<div class="span8">
+      <h1><?php the_title();?></h1>    
+      <div class="row content">
+        <div class="span8">
 
-            <?php the_content();?>
-<?php endwhile; // end of the loop. ?>
+          <?php the_content();?>
+          <?php endwhile; // end of the loop. ?>
 
-<?php// wp_reset_query(); ?>
+          <?php// wp_reset_query(); ?>
 
-<?php
-$args = array( 'post_type' => 'dal_country', posts_per_page => 20 );
-$inscriptionloop = new WP_Query( $args );
-while ( $inscriptionloop->have_posts() ) : $inscriptionloop->the_post();
-	
-	echo '<div class="span well">';		
-	
-	get_template_part('content','dal_inscribete');
+          <?php
+            $args = array( 'post_type' => 'dal_country', posts_per_page => 20 );
+            $inscriptionloop = new WP_Query( $args );
+            while ( $inscriptionloop->have_posts() ) : $inscriptionloop->the_post();
+            	
+            	echo '<div class="span3 well">';		
+            	
+            	get_template_part('content','dal_inscribete');
 
-	echo '</div>';
-                 
-endwhile;
+            	echo '</div>';
+                             
+            endwhile;
 
-?>
+          ?>
 
-          </div><!-- /.span8 -->
+        </div><!-- /.span8 -->
           
           <?php get_sidebar(); ?>
-
+     
 
 <?php get_footer(); ?>
